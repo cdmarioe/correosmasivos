2368a13a54408a3b720e917dc3b39e7165b3a393
cdmarioe <cdmarioe@yahoo.com.mx>
2020-10-16 13:50:52 -0500

    Update mailconf.php

--- mailconf.php
+++ mailconf.php
@@ -2,10 +2,6 @@
 $servidor_smtp = "smtp.gmail.com";
 $puerto_smtp = "465"; 
 $seguridad = "ssl"; // secure transfer ssl/tls o 'vacio' enabled REQUIRED for Gmail
-$usuario_correo = "wire.less.solutions.tj@gmail.com";
-$contrasena_correo = "@Tijuana02";
-//Se puede poner otro correo diferente al del usuario
-//$empresa = "Plomeria";
-//$nombre_quien_envia = "promociones@plomeriadelta.com";
-//$asunto_correo = "Promocion de Internet";
+$usuario_correo = "micorreo@gmail.com"; //cuenta de correo a usar
+$contrasena_correo = "contrasena"; //contrasena de la cuenta de correo
 ?>
